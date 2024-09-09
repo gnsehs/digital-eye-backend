8660f2072b52d74079089803163b318e63c66841
DoHun Han <gks9315@naver.com>
2024-09-09 21:17:10 +0900

    feat: string과 image를 받는 proxy server controller

---
+++ src/main/java/hello/degitaleye/DigitalEyeApplication.java
@@ -0,0 +1,13 @@
+package hello.degitaleye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalEyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalEyeApplication.class, args);
+    }
+
+}
