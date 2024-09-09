758184af3c14b985e9117135ef4e7d911c4de6a3
DoHun Han <gks9315@naver.com>
2024-09-09 21:36:21 +0900

    feat: String 만 받는 api

--- src/main/java/hello/degitaleye/controller/ProxyServerController.java
+++ src/main/java/hello/degitaleye/controller/ProxyServerController.java
@@ -1,7 +1,5 @@
 package hello.degitaleye.controller;
 
-import hello.degitaleye.RestTemplateConfig;
-import hello.degitaleye.domain.TempDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -23,16 +22,41 @@ import java.io.IOException;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-public class MultiPartDataController {
+public class ProxyServerController {
     @Value("${file.dir}")
     private String fileDir;
 
     private final String testUrl = "http://localhost:8080/test_rest_template_get";
     private final RestTemplate restTemplate;
 
+    /**
+     *
+     * @param dialogue text/plain 으로 받는 dialogue
+     * @return
+     */
+    @PostMapping("/tempupload_text")
+    public ResponseEntity<String> tempStringUpload(@RequestBody String dialogue) {
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        log.info("===test0 dialogue = {}===", dialogue);
+        body.add("dialogue", dialogue);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, httpHeaders);
+        ResponseEntity<String> stringResponseEntity = restTemplate.postForEntity(testUrl, requestEntity, String.class);
+        return stringResponseEntity;
+
+    }
+
+    /**
+     *
+     * @param dialogue 사용자 요청
+     * @param file 이미지 파일
+     * @return TODO 프론트엔드에 어떻게 리턴할지 생각하기
+     */
     @PostMapping("/tempupload")
-    public ResponseEntity<String> tempUpload(@RequestPart(value = "dialogue") String dialogue,
-                                             @RequestPart(value = "image", required = false) MultipartFile file) {
+    public ResponseEntity<String> tempFileStringUpload(@RequestPart(value = "dialogue") String dialogue,
+                                                       @RequestPart(value = "image", required = false) MultipartFile file) {
 
         MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
         log.info("===test1 dialogue = {}===", dialogue);
@@ -55,11 +79,14 @@ public class MultiPartDataController {
 
     }
 
+    /*
+    플라스크 서버 역할
+     */
     @PostMapping("/test_rest_template_get")
-    public String test2(@RequestPart(value = "dialogue") String dialogue,
-                        @RequestPart(value = "image") MultipartFile file) {
+    public String tempAiServer(@RequestPart(value = "dialogue") String dialogue,
+                               @RequestPart(value = "image", required = false) MultipartFile file) {
         log.info("===test2 dia = {}===", dialogue);
-        if (!file.isEmpty()) {
+        if (file != null && !file.isEmpty()) {
             String fullPath = fileDir + file.getOriginalFilename();
             log.info("===test2 파일 저장 = {}===", fullPath);
             try {
