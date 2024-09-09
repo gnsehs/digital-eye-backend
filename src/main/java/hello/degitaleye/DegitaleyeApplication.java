ceadf108f9e9fa3dd8ec1c8c730dba9371d02e5e
DoHun Han <gks9315@naver.com>
2024-09-09 19:45:49 +0900

    init commit

---
+++ src/main/java/hello/degitaleye/DegitaleyeApplication.java
@@ -0,0 +1,13 @@
+package hello.degitaleye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DegitaleyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DegitaleyeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/degitaleye/DegitaleyeApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.degitaleye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DegitaleyeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
