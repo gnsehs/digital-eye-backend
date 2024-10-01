776fcf1c13727d4df0bd7e0574bb63f90ccd59bd
DoHun Han <gks9315@naver.com>
2024-10-02 00:25:39 +0900

    feat: 국제화 properties, MessageSource

--- src/main/java/hello/degitaleye/controller/advice/AiExControllerAdvice.java
+++ src/main/java/hello/degitaleye/controller/advice/AiExControllerAdvice.java
@@ -1,7 +1,10 @@
 package hello.degitaleye.controller.advice;
 
 import com.deepl.api.DeepLException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,16 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
+import java.util.Locale;
+
 @RestControllerAdvice(annotations = RestController.class)
 @Slf4j
+@RequiredArgsConstructor
 public class AiExControllerAdvice {
+
+
+    private final MessageSource ms; // 메세지 소스 주입
 
     @ExceptionHandler(DeepLException.class)
     public ResponseEntity<ErrorResult> deepLExHandle(DeepLException e) {
         log.error("[exception-handle] ex", e);
         return ResponseEntity
                 .internalServerError()
-                .body(new ErrorResult(HttpStatus.INTERNAL_SERVER_ERROR.value(), "번역 오류 발생!",""));
+                .body(new ErrorResult(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                        ms.getMessage("error.translate",null,getContextLocale()),
+                        ms.getMessage("solution.translate",null,getContextLocale())));
     }
 
     @ExceptionHandler(MissingServletRequestPartException.class)
@@ -26,7 +37,9 @@ public class AiExControllerAdvice {
         log.error("[exception-handle] ex", e);
         return ResponseEntity
                 .badRequest()
-                .body(new ErrorResult(HttpStatus.BAD_REQUEST.value(), "잘못된 요청 값", "요청 값을 다시 말씀 해 주세요"));
+                .body(new ErrorResult(HttpStatus.BAD_REQUEST.value(),
+                        ms.getMessage("error.400", null, getContextLocale()),
+                        ms.getMessage("solution.translate", null, getContextLocale())));
     }
 
     @ExceptionHandler(Exception.class)
@@ -34,6 +47,12 @@ public class AiExControllerAdvice {
         log.error("[exception-handle] ex", e);
         return ResponseEntity
                 .internalServerError()
-                .body(new ErrorResult(HttpStatus.INTERNAL_SERVER_ERROR.value(), "알 수 없는 오류 발생","요청을 다시 시도해 주세요."));
+                .body(new ErrorResult(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                        ms.getMessage("error.500", null, getContextLocale()),
+                        ms.getMessage("solution.500", null, getContextLocale())));
+    }
+
+    private Locale getContextLocale() {
+        return LocaleContextHolder.getLocale();
     }
 }
