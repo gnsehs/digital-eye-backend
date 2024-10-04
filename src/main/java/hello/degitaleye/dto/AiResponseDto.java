add6f166f74be32109dd6851b44036a2f8500544
DoHun Han <gks9315@naver.com>
2024-10-05 01:44:45 +0900

    chore: AiResponseDto 주석 추가

--- src/main/java/hello/degitaleye/dto/AiResponseDto.java
+++ src/main/java/hello/degitaleye/dto/AiResponseDto.java
@@ -3,6 +3,9 @@ package hello.degitaleye.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * Flask Server 에서 받은 응답을 담는 Dto
+ */
 @Getter
 @NoArgsConstructor
 public class AiResponseDto {
