00339b12aa53bc8c3c9173037284392e0ead3be7
DoHun Han <gks9315@naver.com>
2024-10-09 23:41:16 +0900

    refactor: dto NoArgsConstructor access level 변경

--- src/main/java/hello/degitaleye/dto/AiResponseDto.java
+++ src/main/java/hello/degitaleye/dto/AiResponseDto.java
@@ -1,5 +1,6 @@
 package hello.degitaleye.dto;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,7 +8,7 @@ import lombok.NoArgsConstructor;
  * Flask Server 에서 받은 응답을 담는 Dto
  */
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AiResponseDto {
     private String message;
 
